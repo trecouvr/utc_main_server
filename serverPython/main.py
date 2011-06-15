76685edb07b52fdd5775a30cf82b96b2af451e26
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-06-15 19:12:16 +0200

    début d'exemple de communication

--- serverPython/main.py
+++ serverPython/main.py
@@ -7,25 +7,20 @@ Création du server
 ajout des ports serials
 identification et réarrangement
 """
-
+import os
+import sys
+SERVER_ROOT_DIR  = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+sys.path.append(SERVER_ROOT_DIR)
 
 import subprocess
 import time
-
-#subprocess.Popen([os.path.join(ROOT_DIR,"com","serverPython","kill_socket.sh"),"50000"]).wait()
-#time.sleep(2)
 
 from server import *
 
 server = Server()
 server.start()
 
-#server.addSubprocessClient("clients/python/UDPClient/main.py")
-#server.addSubprocessClient(os.path.join(ROOT_DIR,"Visio","UTCamera","bin","UTCamera"))
-#server.addSubprocessClient(os.path.join(ROOT_DIR,"pinceControl","AX12","scriptPince.py"))
-#server.addSubprocessClient(os.path.join(ROOT_DIR,"clients","soutenance_quentin","main.py"))
-#server.addSubprocessClient(["../../../IA/main.py","1","0"])
-#p = subprocess.Popen(os.path.join(ROOT_DIR,"smartphone.py"))
+#server.addSubprocessClient(os.path.join(SERVER_ROOT_DIR,"clients","C++","a.out"))
 
 
 import glob
